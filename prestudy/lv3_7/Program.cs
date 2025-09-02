53973f8aba1292c1eb0c2aed58f4f4c8ec338812
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2025-09-02 17:10:33 +0900

    250908 step5 퀘스트 마무리

--- prestudy/lv3_7/Program.cs
+++ prestudy/lv3_7/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("행맨 게임을 시작합니다.");
 
-Console.WriteLine("Hello, World!");
+string secretWord = "hangman";
+char[] secretChars = secretWord.ToCharArray();
+char[] guessWord =  new string('_', secretWord.Length).ToCharArray();
+int attempt = 6;
+bool wordGuessed = false;
+
+do
+{
+     Console.WriteLine($"{secretWord.Length}자리 글자를 입력하세요.");
+     string input = Console.ReadLine();
+     char[] inputChar = input.ToCharArray();
+     char[] answer = new string('X', secretWord.Length).ToCharArray();
+
+
+     for (int i = 0; i < answer.Length && i < inputChar.Length; i++)
+     {
+          if (inputChar[i] == secretChars[i])
+          {
+               answer[i] = 'O';
+               guessWord[i] = inputChar[i];
+
+          }
+     }
+     Console.WriteLine($"시행결과 : {new string(answer)}");
+     Console.WriteLine($"guessWord : {new string(guessWord)}");
+     if (guessWord.SequenceEqual(secretChars))
+     {
+          wordGuessed = true;
+     }
+     else
+     {
+          attempt--;
+          Console.WriteLine($"남은 시행횟수 : {attempt}");
+
+     }
+}while (wordGuessed == false && attempt > 0);
+
+if (wordGuessed)
+{
+     Console.WriteLine("축하합니다 정답을 맞췄습니다.");
+}
+else
+{
+     Console.WriteLine("정답을 맞추지 못했습니다.");
+}
--- prestudy/lv3_8/Program.cs
+++ prestudy/lv3_8/Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("야구 게임을 시작합니다.");
+int numCount = 3;
+int [] targetNumber = new int[numCount];
+int [] userGuess = new int[numCount];
+int strikes = 0;
+int balls = 0;
+bool guessedCorrectly = false;
+int attemptCount = 0;
+int generatedCount = 0;
+Random rand = new Random();
 
-Console.WriteLine("Hello, World!");
+while (generatedCount < numCount)
+{
+    int newDigit = rand.Next(0, 10); // 0부터 9까지의 난수 생성
+
+    // 이미 생성된 숫자인지 확인 (중복 방지)
+    if (!targetNumber.Contains(newDigit))
+    {
+        targetNumber[generatedCount] = newDigit;
+        generatedCount++;
+    }
+}
+
+do
+{
+    strikes = 0;
+    balls = 0;
+    Console.WriteLine("Enter your guess (3 digits) : ");
+    int num = inputNumber();
+    userGuess[0] = num / 100;
+    userGuess[1] = (num / 10) % 10;
+    userGuess[2] = num % 10;
+    attemptCount++;
+    for (int i = 0; i < numCount; i++)
+    {
+        for (int j = 0; j < numCount; j++)
+        {
+            if (userGuess[i] == targetNumber[j])
+            {
+                if (i == j) strikes++;
+                else balls++;
+            }
+        }
+    }
+
+    if (strikes == 3)
+    {
+        guessedCorrectly = true;
+        Console.WriteLine($"Congratulations! You've guessed the number in {attemptCount} attempts.");
+    }
+    else
+    {
+        Console.WriteLine($"{strikes} Strike(s),{balls} Ball(s)");
+    }
+
+
+}while(!guessedCorrectly);
+
+int inputNumber()
+{
+    while (true)
+    {
+        Console.Write("3자리 숫자를 입력해주세요: ");
+        string userInput = Console.ReadLine();
+
+        if (userInput.Length != 3)
+        {
+            Console.WriteLine("입력 길이가 3자리가 아닙니다. 다시 시도해주세요.");
+            continue;
+        }
+
+        if(int.TryParse(userInput, out int number))
+        {
+            if (number > 0) {
+                return number;
+            }
+        }
+        Console.WriteLine("유효한 3자리 숫자가 아닙니다. 다시 시도해주세요.");
+    }
+}
